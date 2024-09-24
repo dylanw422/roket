e3f38a953965e954f44dc15d3a9bebf2faeda1d2
dylanw422 <dylanwestdev@gmail.com>
2024-09-24 16:46:47 -0500

    tauri config to open new browser window

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,8 +1,19 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
+use tauri::api::shell::open;
+use tauri::{Window, Manager};  // Import the Manager trait for shell_scope
+
+// Mark the function as a Tauri command
+#[tauri::command]
+fn open_in_browser(window: Window, url: String) {
+    // Use the shell_scope method from the Manager trait
+    open(&window.shell_scope(), url, None).unwrap();
+}
 
 fn main() {
-  tauri::Builder::default()
-    .run(tauri::generate_context!())
-    .expect("error while running tauri application");
+    tauri::Builder::default()
+        .invoke_handler(tauri::generate_handler![open_in_browser])
+        .run(tauri::generate_context!())
+        .expect("error while running tauri application");
 }
+
