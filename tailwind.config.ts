c24b1aec44a2efad6b277c7c4c98192e2eb53e57
dylanw422 <dylanwestdev@gmail.com>
2024-10-02 14:03:05 -0500

    added neon db

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -72,6 +72,10 @@ const config = {
           "0%": { transform: "translateX(100%)", opacity: "0" },
           "100%": { transform: "translateX(0)", opacity: "1" },
         },
+        slideOut: {
+          "0%": { transform: "translateX(0)", opacity: "1" },
+          "100%": { transform: "translateX(100%)", opacity: "0" },
+        },
         slideUp: {
           "0%": { transform: "translateY(100%)", opacity: "0" },
           "100%": { transform: "translateY(0)", opacity: "1" },
@@ -95,6 +99,7 @@ const config = {
         "accordion-down": "accordion-down 0.2s ease-out",
         "accordion-up": "accordion-up 0.2s ease-out",
         slideIn: "slideIn 0.5s ease-out forwards",
+        slideOut: "slideOut 0.5s ease-out forwards",
         slideUp: "slideUp 0.5s ease-out forwards",
         appear: "appear 0.5s ease-out",
         gradient: "gradient 8s linear infinite",
