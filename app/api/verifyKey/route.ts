39b1ba2f4edc7d91048285c7ca9d6859b3d681f5
dylanw422 <dylanwestdev@gmail.com>
2024-10-08 16:36:22 -0500

    removed cookies

--- app/api/verifyKey/route.ts
+++ app/api/verifyKey/route.ts
@@ -1,20 +1,10 @@
 import { neon } from "@neondatabase/serverless";
 import { machineIdSync } from "node-machine-id";
-import { cookies } from "next/headers";
 
 export async function POST(req: Request) {
   const reqBody = await req.json();
   const sql = neon(process.env.DATABASE_URL!);
-  const cookieStore = cookies();
-
-  const savedKey = cookieStore.get("product_key")?.value;
-
-  let key;
-  if (savedKey) {
-    key = savedKey;
-  } else {
-    key = reqBody.key;
-  }
+  const key = reqBody.key;
 
   const verify =
     await sql`SELECT * FROM product_keys WHERE product_key = ${key}`;
@@ -41,10 +31,6 @@ export async function POST(req: Request) {
       [deviceId, currentTimestamp, key],
     );
 
-    cookieStore.set("product_key", key, {
-      expires: new Date(Date.now() + 1000 * 60 * 60 * 24 * 365),
-    });
-
     return Response.json(
       { success: true, message: `New key verified for device ${deviceId}` },
       { status: 200 },
@@ -54,9 +40,6 @@ export async function POST(req: Request) {
   if (verify[0].used === true) {
     const deviceId = machineIdSync();
     if (deviceId === verify[0].deviceid) {
-      cookieStore.set("product_key", key, {
-        expires: new Date(Date.now() + 1000 * 60 * 60 * 24 * 365),
-      });
       const currentTimestamp = new Date();
       await sql(
         "UPDATE product_keys SET last_signin = $1 WHERE deviceid = $2",
